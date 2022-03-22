5c1d2044854672d7956600b35e1de3e66e070cb3
YAMY1234 <2682000734@qq.com>
2022-03-22 22:09:04 +0800

    Site updated: 2022-03-22 22:09:04

---
+++ img/批量重命名脚本.py
@@ -0,0 +1,18 @@
+import os
+path = r"D:/Program_Files_Resources/hexo2/themes/Butterfly/source/img/新来的网络图片/"
+# 获取该目录下所有文件，存入列表中
+f = os.listdir(path)
+print(len(f))
+
+print(f[0])
+pre_n = 17
+n = 0
+for i in f:
+    # 设置旧文件名（就是路径+文件名）
+    oldname = f[n]
+    # 设置新文件名
+    newname = str(n+pre_n) + '.jpg'
+    # 用os模块中的rename方法对文件改名
+    os.rename(path+oldname, path+newname)
+    print(oldname, '======>', newname)
+    n += 1
